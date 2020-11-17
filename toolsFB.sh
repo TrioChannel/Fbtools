e4d17ccfcbb6efdfaf14e1efc9268800fb515221
TrioChannel <72772686+TrioChannel@users.noreply.github.com>
2020-11-17 11:03:12 +0700

    Create toolsFB.sh

---
+++ toolsFB.sh
@@ -0,0 +1,117 @@
+#gak usah di ricord cuy
+#gua kesel ngetik bgsat
+#usr/bin/bash
+clear
+bi='\33[34;1m' #biru
+ij='\33[32;1m' #ijo
+pr='\33[35;1m' #purple
+cy='\33[36;1m' #cyan
+me='\33[31;1m' #merah
+pu='\33[37;1m' #putih
+ku='\33[33;1m' #kuning
+echo
+sleep 1
+echo """
+                    $ku Sabar Kuy Saya itung Sampai 3"""
+sleep 1
+echo """                                 ____
+                        $ku     |_   |
+                        $ku      _|  |_
+                        $ku     |______|"""
+sleep 1
+echo """                $ku               ___
+                                 |__ \
+                                    ) |
+                                   / /
+                                  / /_
+                                 |____|"""
+sleep 1
+echo """                                 ____
+                        $ku      |___ \
+                                  __) |
+                                  |__ <
+                                  ___) |
+                                 |____/"""
+sleep 2
+echo $ku"               Sudah Cuy :)"
+sleep 3
+clear
+echo
+echo $cy "╔═══════════════════════════════════════════════╗"
+echo $cy "║►"$pr"Author : TRIO CHANNEL."$cy"                        ║"
+echo $cy "║►"$pr"Youtube : TRIO CHANNEL."$cy"                       ║"
+echo $cy "║►"$pr"Github :https://github.com/TrioChannel."$cy"       ║"
+echo $cy "║►"$pr"Selamat  Mencoba Tools nya."$cy"                   ║"
+echo $cy "╚═══════════════════════════════════════════════╝"
+echo
+echo $ku "Silahkan Pilih Jika kena Cp Saya Tidak Betanggung Jawab" #By Trio Channel
+echo $cy "╔═══════════════╗"
+echo $cy "╠═══════════════"
+echo $cy "║[1],Hack FB v1"
+echo $cy "╠═══════════════"
+echo $cy "║[2],Hack FB v2"
+echo $cy "╠═══════════════"
+echo $cy "║[3],Dark Fb New"
+echo $cy "╠═══════════════"
+echo $cy "║[4],Hack FB v3"
+echo $cy "╠═══════════════"
+echo $cy "║[5],Hack Fb v4 ( Rename : Anak,pw : Bang"
+echo $cy "╠═══════════════"
+echo $cy "╚════════════════╝"
+echo
+read -p "Silahkan Pilih : " pil :
+
+#Apa Luu tod 111111111111
+if [ $pil = 1 ]
+then
+clear
+echo $ku"[•]L O A D I N G• • • •"
+sleep 1
+git clone https://github.com/Bl4ckDr460n/Black-Fb-Premium
+cd Balck-Fb-Premium
+python2 Black-Fb.py
+fi
+#Ngapain todd 11111111
+if [ $pil = 2 ]
+then
+clear
+echo $ku"[•]L O A D I N G• • • •"
+sleep 1
+git clone https://github.com/rezadkim/MBFz
+cd MBFz
+pip2 install -r nganunymous.txt
+pip2 install mechanize
+pip2 install progress
+python2 mbfz.py
+fi
+#ngapain todb11111qq
+if [ $pil = 3 ]
+then
+clear
+echo $ku"[•]L O A D I N G• • • •"
+sleep 1
+git clone https://github.com/ARIYA-CYBER/NEW
+cd NEW
+python2 FbNew.py
+fi
+#ngapin tod 1111111111
+if [ $pil = 4 ]
+then
+clear
+echo $ku"[•]L O A D I N G• • • •"
+sleep 1
+git clone https://github.com/blackcodercrush/hack-facebook-teman
+cd hack-facebook-teman
+sh requests.sh
+python2 hack-fb.py
+fi
+#nagapin tod 1111111
+if [ $pil = 3 ]
+then
+clear
+echo $ku"[•]L O A D I N G• • • •"
+sleep 1
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh v2.sh
+fi
